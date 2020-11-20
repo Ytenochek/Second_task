ceacce8d68120c7a56f150666b55784c748f6c22
Ytenochek <konal2004@gmail.com>
2020-11-20 15:28:28 +0500

    DONE

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+from random import randint
 
 from PyQt5 import uic
 from PyQt5.QtGui import QPainter, QColor
@@ -9,7 +10,8 @@ class App(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi("UI.ui", self)
-        self.border = (600, 450)
+        self.border_down = (50, 50)
+        self.border_up = (600, 450)
         self.draw.clicked.connect(self.paint)
 
     def paintEvent(self, event):
@@ -31,6 +33,9 @@ class App(QMainWindow):
 
     def draw_circle(self, qp):
         qp.setBrush(QColor(255, 255, 0))
+        center = (randint(self.border_down[0], self.border_up[0]), randint(self.border_down[1], self.border_up[1]))
+        radius = randint(5, 100)
+        qp.drawEllipse(*center, radius, radius)
 
 
 
