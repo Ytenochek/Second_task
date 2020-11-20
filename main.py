b68406afda2209959acf4f06fcba65ebfe2c727a
Ytenochek <konal2004@gmail.com>
2020-11-20 15:23:52 +0500

    Almost done

--- main.py
+++ main.py
@@ -9,18 +9,32 @@ class App(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi("UI.ui", self)
+        self.border = (600, 450)
+        self.draw.clicked.connect(self.paint)
 
     def paintEvent(self, event):
-        # Создаем объект QPainter для рисования
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_flag(qp)
+            qp.end()
+
+    def paint(self):
+        self.do_paint = True
+        self.repaint()
+
+    def paintEvent(self, event):
         qp = QPainter()
-        # Начинаем процесс рисования
         qp.begin(self)
-        self.draw_flag(qp)
-        # Завершаем рисование
+        self.draw_circle(qp)
         qp.end()
 
+    def draw_circle(self, qp):
+        qp.setBrush(QColor(255, 255, 0))
 
-if __name__ == '__main__':
+
+
+if __name__ == "__main__":
     app = QApplication(sys.argv)
     ex = App()
     ex.show()
